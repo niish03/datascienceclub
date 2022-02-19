f880c10fe8161e8c36103c0eae854a2cfb8f4a7e
NISHIT PATEL <61544937+niish03@users.noreply.github.com>
2022-02-19 18:10:15 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0px";
+  } else {
+    document.getElementById("navbar").style.top = "-210px";
+  }
+
+  prevScrollpos = currentScrollPos;
+}
+
+document.getElementById('navbar-mobile').addEventListener("click", () => {
+  if (window.innerWidth < 990)
+    document.getElementById('navbar-toggler').click();
+})
+document.getElementById("navbar-toggler").addEventListener('click', () => {
+    $('i[name="appsicon"]').removeClass('Visually-hidden');
+  
+  });
+
